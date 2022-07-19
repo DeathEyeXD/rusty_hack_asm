460c518475ebff93b008cec12f327c476222eb22
Karol Zwolak <deatheyex132@gmail.com>
2022-07-19 16:54:13 +0200

    Add scanner error handling

--- src/scanner.rs
+++ src/scanner.rs
@@ -2,12 +2,47 @@ use std::fs;
 
 use self::token::Token;
 mod token;
+
+struct ScanningError{
+    message: String,
+}
+impl ScanningError {
+    fn new(message: &str, source: &str, start: usize, len: usize, line: usize) -> ScanningError {
+        let highlight = Self::highlight(source, start, len, line);
+        ScanningError{
+            message:  format!("{}\n     {}", highlight, message),
+        }
+    }
+
+    fn get_line_contents<'a>(source: &'a str, start: usize) -> (&'a str, usize){
+        let start = source[..start].bytes().rev().position(|c| c == b'\n').unwrap_or(0);
+        let end = source[start..].bytes().position(|c| c == b'\n').unwrap_or_else(|| start.saturating_sub(1));
+        
+        (&source[start..end], start)
+    }
+
+    fn highlight(source: &str, start: usize, len: usize, line: usize) -> String{
+        let lexeme = &source[start..start+len];
+        let (line_contents, line_start) = Self::get_line_contents(source, start);
+        let padding_len = line / 100 + 4 + start - line_start; 
+        format!("{} | {}\n", line, line_contents) + line_contents + &format!("{}{}--here"," ".repeat(padding_len) ,"^".repeat(len))
+    }
+}
+impl std::fmt::Display for ScanningError{
+
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}",self.message)
+    }
+
+}
+
 struct Scanner{
     source: String,
     tokens: Vec<Token>,
     curr: usize,
     start: usize,
     line: usize,
+    errors: Vec<ScanningError>,
 }
 
 impl Scanner {
@@ -19,6 +54,7 @@ impl Scanner {
             curr: 0,
             line: 1,
             start: 0,
+            errors: Vec::new(),
         }
     }
 
@@ -114,7 +150,8 @@ impl Scanner {
             b'/' => {
                 if self.match_next(b'/') {
                 self.skip_comment();
-                } else { //TODO raise an error
+                } else { 
+                    self.raise_error("Unexpected character")
                 }
             },
             b';' => self.add_token(token::TokenType::Semicolon),
@@ -141,6 +178,14 @@ impl Scanner {
         }
         self.tokens.push(Token::new(token::TokenType::Eof, self.line, self.start));
         &self.tokens
+    }
+
+    fn had_errors(&self) -> bool {
+        !self.errors.is_empty()
+    }
+
+    fn raise_error(&mut self, message: &str){
+        self.errors.push(ScanningError::new(message, &self.source, self.start, self.curr-self.start, self.line));
     }
 }
 
