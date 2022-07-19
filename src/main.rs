b1ef8053e87b122d3dcbbf938b670e7dd817b923
Karol Zwolak <deatheyex132@gmail.com>
2022-07-19 14:04:42 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+mod scanner;
+
+pub type Error = Box<dyn std::error::Error>;
+pub type Result<T> = std::result::Result<T, Error>;
+struct Assembler{
+
+}
+fn main() {
+    println!("Hello, world!");
+}
